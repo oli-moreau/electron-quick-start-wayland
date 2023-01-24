09c6f72ed27b94e73469f871bba6aeacafa14392
Olivier Moreau <moreau.olivier@outlook.com>
2023-01-24 14:18:48 -0500

    Modify renderer.js
    Add:
    - ipcRenderer
    - Minimize, maximize, close functions

--- renderer.js
+++ renderer.js
@@ -5,3 +5,26 @@
  * `contextIsolation` is turned on. Use the contextBridge API in `preload.js`
  * to expose Node.js functionality from the main process.
  */
+
+//Declarations
+const ipc = require('electron').ipcRenderer
+
+//Minimize
+function minimizeApp() {
+  ipc.send('minimize')
+}
+
+//Maximize
+function maximizeApp() {
+  ipc.send('maximize')
+}
+
+//Close
+function closeApp() {
+  ipc.send('close')
+}
+
+//Events
+document.querySelector('.close').addEventListener('click', closeApp)
+document.querySelector('.maximize').addEventListener('click', maximizeApp)
+document.querySelector('.minimize').addEventListener('click', minimizeApp)
